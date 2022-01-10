875c8a6fc116bd977a62e2f406181aa3aaa7ad6d
kaiware710 <yu.sskabc.my123@gmail.com>
2022-01-10 15:56:10 +0900

    udemy-26:構造体

--- TodoApp/TodoApp/tutorial.playground/Contents.swift
+++ TodoApp/TodoApp/tutorial.playground/Contents.swift
@@ -1,26 +1,32 @@
-class Animal {
-    var age = 2
-    let kind = "犬"
-
+struct Animal {
+    var age = 5
+    var kind = "犬"
     func Bite() {
-        print("\(age)歳の\(kind)が噛み付いてきた。")
+        print("\(age)歳の\(kind)が噛み付く")
     }
 }
 var dog = Animal()
-print(String(dog.age)+"歳")
 dog.Bite()
 
 
-class AnimalInit {
+struct AnimalInit {
     var age: Int
     var kind: String
     func Bite() {
-        print("\(age)歳の\(kind)が噛み付いてきた。")
+        print("\(age)歳の\(kind)が噛み付く")
     }
     init(age: Int, kind: String) {
         self.age = age
         self.kind = kind
     }
 }
-var cat = AnimalInit(age: 4, kind: "猫")
-cat.Bite()
+var panda = AnimalInit(age: 11, kind: "パンダ")
+//panda.Bite()
+
+
+var animals: [AnimalInit] = [
+    AnimalInit(age: 10, kind: "ライオン"),
+    AnimalInit(age: 17, kind: "オランウータン"),
+    AnimalInit(age: 20, kind: "ヒト"),
+]
+animals[2].Bite()
