a7bcee8070031640370aa22fc730f959ba6fc668
kaiware710 <yu.sskabc.my123@gmail.com>
2022-01-13 21:41:01 +0900

    27:プロトコル

--- TodoApp/TodoApp/tutorial.playground/Contents.swift
+++ TodoApp/TodoApp/tutorial.playground/Contents.swift
@@ -1,32 +1,11 @@
-struct Animal {
-    var age = 5
-    var kind = "犬"
-    func Bite() {
-        print("\(age)歳の\(kind)が噛み付く")
+protocol Animal {
+    var age: Int { get }  // プロパティ
+    func bark()   // メソッド
+}
+
+struct Dog: Animal {
+    let age: Int
+    func bark() {
+        print("犬が吠える")
     }
 }
-var dog = Animal()
-dog.Bite()
-
-
-struct AnimalInit {
-    var age: Int
-    var kind: String
-    func Bite() {
-        print("\(age)歳の\(kind)が噛み付く")
-    }
-    init(age: Int, kind: String) {
-        self.age = age
-        self.kind = kind
-    }
-}
-var panda = AnimalInit(age: 11, kind: "パンダ")
-//panda.Bite()
-
-
-var animals: [AnimalInit] = [
-    AnimalInit(age: 10, kind: "ライオン"),
-    AnimalInit(age: 17, kind: "オランウータン"),
-    AnimalInit(age: 20, kind: "ヒト"),
-]
-animals[2].Bite()
