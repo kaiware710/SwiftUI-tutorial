0907b67e18f0c065f90231b661841c8fbf262f11
kaiware710 <yu.sskabc.my123@gmail.com>
2022-01-15 18:02:04 +0900

    40:Button

--- TodoApp/TodoApp/ContentView.swift
+++ TodoApp/TodoApp/ContentView.swift
@@ -6,26 +6,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView{
-            List {
-                Text("おはよう")
-                    .foregroundColor(Color.red)
-                    .padding()
-                    .font(.body)
-                Text("こんにちは")
-                    .foregroundColor(Color.blue)
-                    .padding()
-                    .font(.caption)
-                Text("こんばんは")
-                    .foregroundColor(Color.gray)
-                    .padding()
-                    .font(.subheadline)
-            }
-                .navigationBarTitle(Text("挨拶"))
-                .navigationBarItems(trailing:
-                    Text("言葉")
-                )
-        }
+//        NavigationView{
+//            List {
+//                Text("おはよう")
+//                    .foregroundColor(Color.red)
+//                    .padding()
+//                    .font(.body)
+//                Text("こんにちは")
+//                    .foregroundColor(Color.blue)
+//                    .padding()
+//                    .font(.caption)
+//                Text("こんばんは")
+//                    .foregroundColor(Color.gray)
+//                    .padding()
+//                    .font(.subheadline)
+//            }
+//                .navigationBarTitle(Text("挨拶"))
+//                .navigationBarItems(trailing:
+//                    Text("言葉")
+//                )
+//        }
 //        HStack {
 //            Text("Japanese Greetings")
 //            Text("日本語の挨拶")
@@ -35,8 +35,16 @@ struct ContentView: View {
 //                .padding()
 //            Text("Hello")
 //        }
+        Button(action: {print("ボタン押下")}) {
+            Text("クリック！")
+        }
     }
 }
+
+func Report() {
+    print("ボタン押下")
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
