d21898802c5cbb16aa88c6f8238694fa4210fcd0
kaiware710 <yu.sskabc.my123@gmail.com>
2022-01-15 17:06:10 +0900

    39:Font

--- TodoApp/TodoApp/ContentView.swift
+++ TodoApp/TodoApp/ContentView.swift
@@ -6,32 +6,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        NavigationView{
-//            List {
-//                Text("おはよう")
-//                    .foregroundColor(Color.red)
-//                    .padding()
-//                Text("こんにちは")
-//                    .foregroundColor(Color.blue)
-//                    .padding()
-//                Text("こんばんは")
-//                    .foregroundColor(Color.gray)
-//                    .padding()
-//            }
-//                .navigationBarTitle(Text("挨拶"))
-//                .navigationBarItems(trailing:
-//                    Text("言葉")
-//                )
-//        }
+        NavigationView{
+            List {
+                Text("おはよう")
+                    .foregroundColor(Color.red)
+                    .padding()
+                    .font(.body)
+                Text("こんにちは")
+                    .foregroundColor(Color.blue)
+                    .padding()
+                    .font(.caption)
+                Text("こんばんは")
+                    .foregroundColor(Color.gray)
+                    .padding()
+                    .font(.subheadline)
+            }
+                .navigationBarTitle(Text("挨拶"))
+                .navigationBarItems(trailing:
+                    Text("言葉")
+                )
+        }
 //        HStack {
 //            Text("Japanese Greetings")
 //            Text("日本語の挨拶")
 //        }
-        VStack {
-            Text("Good Morning")
-                .padding()
-            Text("Hello")
-        }
+//        VStack {
+//            Text("Good Morning")
+//                .padding()
+//            Text("Hello")
+//        }
     }
 }
 
