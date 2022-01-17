1b7eb2b3c545c183de2a3513e87e8366431c2ac0
kaiware710 <yu.sskabc.my123@gmail.com>
2022-01-17 18:39:26 +0900

    画像分類アプリ:request作成

--- Resnet50/Resnet50/ContentView.swift
+++ Resnet50/Resnet50/ContentView.swift
@@ -3,11 +3,47 @@
 //  Created by 國分悠輔 on 2022/01/17.
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct ContentView: View {
+    
+    @State var classificationLabel = ""
+    
+    // リクエスト作成
+    func createClassificationRequest() -> VNCoreMLRequest {
+        do {
+            let configuration = MLModelConfiguration()
+            
+            let model = try VNCoreMLModel(  // VNCoreMLModelはモデルが読み込まれずにエラー投げることあり
+                for: Resnet50(configuration: configuration).model
+            )
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: {
+                request, error in
+                    performClassification(request: request)
+            })
+            
+            return request
+            
+        } catch {  // アプリ強制終了
+            fatalError("modlが読み込めません。")
+        }
+    }
+    
+    func performClassification(request: VNRequest) {
+        guard let results = request.results else {
+            return
+        }
+        
+        let classification = results as! [VNClassificationObservation]
+        
+        classificationLabel = classification[0].identifier
+    }
+    
     var body: some View {
         VStack {
-            Text("猫の画像")
+            Text(classificationLabel)
                 .padding()
                 .font(.title)
             Image("cat1")
